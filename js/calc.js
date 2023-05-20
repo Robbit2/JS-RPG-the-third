48f446105a623cc80bac3f91acc5a021980d8f46
Robbit2 <59852162+Robbit2@users.noreply.github.com>
2023-05-20 17:32:19 +0000

    added calculation class and entity class

---
+++ js/calc.js
@@ -0,0 +1,16 @@
+class calculation {
+    initialDamage ({weaponDamage, strength}) {
+        let damage = ( 1 + weaponDamage ) * ( strength / 100 );
+        return damage;
+    }
+
+    damageMultiplier ({combatLevel, weaponBonus, enchants}) {
+        let multiplier = 1 + ( combatLevel * 0.02 ) + enchants + weaponBonus;
+        return mulitplier;
+    }
+
+    finalDamage ({initial_Damage, damage_Multiplier, armorBonus}) {
+        let damage = initial_Damage * damage_Multiplier * (1 + armorBonus);
+        return damage;
+    }
+}
---
+++ js/entity.js
@@ -0,0 +1,14 @@
+class Entity {
+    constructor ({name, level, maxHealth, health}) {
+        this.name = name;
+        this.level = level;
+        this.maxHealth = maxHealth;
+        this.health = health;
+    }
+
+    damage (amount) {
+        this.health
+    }
+  
+}
+
